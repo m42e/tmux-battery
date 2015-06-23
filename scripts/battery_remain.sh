d7afc6e769db496c88f42bd3a32b4b8c1ad89bd1
Matthias Bilger <Matthias.Bilger@bmw.de>
2015-06-23 19:57:21 +0200

    Added windows/cygwin method

--- scripts/battery_remain.sh
+++ scripts/battery_remain.sh
@@ -9,6 +9,11 @@ battery_discharging() {
 	[[ $status =~ (discharging) ]]
 }
 
+battery_charging() {
+	local status="$(battery_status)"
+	[[ $status =~ (charging) ]]
+}
+
 pmset_battery_remaining_time() {
 	local output="$(pmset -g batt | awk 'NR==2 { gsub(/;/,""); print $4 }')"
 	# output has to match format "10:42"
@@ -18,19 +23,33 @@ pmset_battery_remaining_time() {
 }
 
 print_battery_remain() {
-	if command_exists "pmset"; then
-		pmset_battery_remaining_time
-	elif command_exists "upower"; then
-		battery=$(upower -e | grep battery | head -1)
-		upower -i $battery | grep remain | awk '{print $4}'
-	elif command_exists "acpi"; then
-		acpi -b | grep -Eo "[0-9]+:[0-9]+:[0-9]+"
-	fi
+   if is_cygwin; then
+      wimc_get_Battery EstimatedRuntime | awk '{print int($1/60) ":" $1%60}'
+   else
+      if command_exists "pmset"; then
+         pmset_battery_remaining_time
+      elif command_exists "upower"; then
+         battery=$(upower -e | grep battery | head -1)
+         upower -i $battery | grep remain | awk '{print $4}'
+      elif command_exists "acpi"; then
+         acpi -b | grep -Eo "[0-9]+:[0-9]+:[0-9]+"
+      fi
+   fi
+}
+
+print_till_full() {
+   if is_cygwin; then
+      wimc_get_Battery EstimatedChargeRemaining | awk '{print int($1/60) ":" $1%60}'
+   else
+      echo ""
+   fi
 }
 
 main() {
 	if battery_discharging; then
 		print_battery_remain
+   elif battery_charging; then
+      print_till_full
 	fi
 }
 main
